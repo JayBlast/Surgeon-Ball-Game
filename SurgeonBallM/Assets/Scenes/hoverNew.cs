5a111d73543cac36dcf9a4cf5fab9bdb04588bb3
juliangh6 <julian.mcgloth@gmail.com>
2020-12-01 19:18:31 -0600

    SurgeonBallM
    The unity enviornment and project (preparing for live demo 2)

---
+++ SurgeonBallM/Assets/Scenes/hoverNew.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverNew : MonoBehaviour
+{
+    private CharacterController _controller;
+    public float speed = 3.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * speed);
+    }
+}
