02c8c3e2800d1b61a69141f7745442c6b2f07ff1
juliangh6 <julian.mcgloth@gmail.com>
2021-02-02 19:26:03 -0600

    Movement $ respawn
    Prep for demo

---
+++ SurgeonBallM/Assets/Scenes/DEFUNCT_hoverNew.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class hoverNew : MonoBehaviour
+{
+    private CharacterController _controller;
+    //public CharacterController _controller;
+    public float speed = 3.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * speed);*/
+        float moveSpeed = 10;
+        //Define the speed at which the object moves.
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //Get the value of the Horizontal input axis.
+
+        float verticalInput = Input.GetAxis("Vertical");
+        //Get the value of the Vertical input axis.
+
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime);
+        //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+    }
+}
---
+++ SurgeonBallM/Assets/Scenes/GoalPointScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalPointScript : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            //later add functionality that ends level when all of this object is destroyed
+        }
+    }
+}
