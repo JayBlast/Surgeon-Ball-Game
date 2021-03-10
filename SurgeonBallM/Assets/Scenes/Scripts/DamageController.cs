fcb8eb168825ce039c5440c9d5c2a476aebe844a
juliangh6 <julian.mcgloth@gmail.com>
2021-03-09 21:51:24 -0600

    Damage Zones, and goal zones reimplemented

---
+++ SurgeonBallM/Assets/Scenes/Scripts/DamageController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageController : MonoBehaviour
+{
+    public float playerDamage;//Variable for how much health the player has
+    [SerializeField] public Text damageText;
+    public bool isDamaging;
+    [SerializeField] public Text checkDamage;
+
+    private void Start()
+    {
+        UpdateDamage();
+    }
+    public AccuracyCalculator myAcc;
+
+    public void UpdateDamage()
+    {
+       if (isDamaging == true)
+       {
+            playerDamage = playerDamage + 0.1f;//slowly increments the damage
+       }
+        checkDamage.text = isDamaging.ToString();
+        damageText.text = "Damage: [" + playerDamage.ToString("0.0") + "]";//updates the text connected to this controller, as a float
+        myAcc.UpdateAcc();
+    }
+}
+    
---
+++ SurgeonBallM/Assets/Scenes/Scripts/GoalZones.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZones : MonoBehaviour
+{
+    [SerializeField] public DamageController myDamage;
+    //private bool sArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sArea = true;//defaults the players position within the surgical area
+        myDamage.isDamaging = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myDamage.isDamaging = false; //Turns on the damaging function on the AccuracyCalculator script
+            myDamage.UpdateDamage();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myDamage.isDamaging = true;//Turns off the damaging function on the AccuracyCalculator script
+            myDamage.UpdateDamage();
+        }
+    }
+
+    void Update()
+    {
+        if (myDamage.isDamaging)
+            myDamage.UpdateDamage(); 
+    }
+
+}
